9691dd9e04485d059d2323764dd2ea65f348ea51
tsodre-p <tsodre-p@student.42porto.com>
2023-06-07 15:39:32 +0100

    fixing echo

--- src/built-ins/echo.c
+++ src/built-ins/echo.c
@@ -6,7 +6,7 @@
 /*   By: tsodre-p <tsodre-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 10:15:06 by tsodre-p          #+#    #+#             */
-/*   Updated: 2023/06/07 15:31:26 by tsodre-p         ###   ########.fr       */
+/*   Updated: 2023/06/07 15:39:17 by tsodre-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ char	*clean_quotes(char *input)
 	{
 		if (input[0] == '\'' || input[0] ==  '"')
 		{
-			if (input[i] != '\'' || input[i] != '"')
+			if (!(input[i] == '\'') || !(input[i] == '"'))
 			{
 				string[j] = input[i];
 				j++;
